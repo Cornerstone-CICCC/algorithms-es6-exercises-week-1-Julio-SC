38ca679fc86e4e786800fab31a83540be472357d
Julio-SC <jhver2001@gmail.com>
2024-09-18 23:50:08 -0700

    Exercise 10 complete

--- exercises/ex10.js
+++ exercises/ex10.js
@@ -1,10 +1,6 @@
-/*
-Rewrite the following while loop as a for loop:
+//Rewrite the following while loop as a for loop://
 
 let x = 9;
-while (x >= 1) {
-  console.log("hello " + x);
-  x = x - 1;
+for (x >= 1; x = x - 1; ) {
+  console.log("hello " + x) 
 }
-
-*/
